4a8e938448485e4a9e645eee3e84840e7473e22c
nathanmendis <nathanmendis17@gmail.com>
2025-05-05 00:25:32 +0530

    ann1 commit

--- ANN/1.py
+++ ANN/1.py
@@ -1,9 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-print("Natha Mendis TE-AIML 33543")
-print("script started")
-x = np.linspace(-10, 10, 100)
 
+# Define activation functions
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
@@ -13,15 +11,38 @@ def tanh(x):
 def relu(x):
     return np.maximum(0, x)
 
-def leaky_relu(x):
-    return np.where(x > 0, x, 0.01 * x)
+def leaky_relu(x, alpha=0.01):
+    return np.where(x > 0, x, alpha * x)
 
-plt.figure(figsize=(10, 6))
-plt.plot(x, sigmoid(x), label="Sigmoid")
-plt.plot(x, tanh(x), label="Tanh")
-plt.plot(x, relu(x), label="ReLU")
-plt.plot(x, leaky_relu(x), label="Leaky ReLU")
-plt.title("Activation Functions")
-plt.legend()
+# X-axis values
+x = np.linspace(-10, 10, 400)
+
+# Create subplots: 2 rows, 2 columns
+plt.figure(figsize=(12, 8))
+
+# 1. Sigmoid
+plt.subplot(2, 2, 1)
+plt.plot(x, sigmoid(x), color='blue')
+plt.title("Sigmoid")
 plt.grid(True)
+
+# 2. Tanh
+plt.subplot(2, 2, 2)
+plt.plot(x, tanh(x), color='green')
+plt.title("Tanh")
+plt.grid(True)
+
+# 3. ReLU
+plt.subplot(2, 2, 3)
+plt.plot(x, relu(x), color='red')
+plt.title("ReLU")
+plt.grid(True)
+
+# 4. Leaky ReLU
+plt.subplot(2, 2, 4)
+plt.plot(x, leaky_relu(x), color='purple')
+plt.title("Leaky ReLU")
+plt.grid(True)
+
+plt.tight_layout()
 plt.show()
